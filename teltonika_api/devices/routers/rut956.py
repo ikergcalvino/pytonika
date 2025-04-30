28b5c7e6b3258db11fed7ad13a268435ef904a1f
Iker García Calviño <iker.gcalvino@udc.es>
2025-04-30 10:39:19 +0200

    Add RUT240 and RUT956 device classes with endpoint integration

---
+++ teltonika_api/devices/routers/rut956.py
@@ -0,0 +1,21 @@
+from .router import Router
+from ...endpoints import Users, Firmware, Interfaces
+
+
+class RUT956(Router):
+    def __init__(self, base_url: str):
+        super().__init__(base_url)
+
+        self.users = Users(self._api_client)
+        self.firmware = Firmware(self._api_client)
+        self.interfaces = Interfaces(self._api_client)
+
+    def __getattr__(self, attr):
+        for endpoint in [self.users,
+                         self.firmware,
+                         self.interfaces]:
+
+            if hasattr(endpoint, attr):
+                return getattr(endpoint, attr)
+
+        return super().__getattr__(attr)
