ade5c55d3706dd76082d0a1535fba7feaff017d1
Iker García Calviño <iker.gcalvino@udc.es>
2025-04-30 11:03:08 +0200

    Add WireGuard endpoint class with CRUD operations and key generation

---
+++ teltonika_api/endpoints/wireguard.py
@@ -0,0 +1,54 @@
+from typing import Any, Dict, List, Tuple, Union
+
+
+class WireGuard:
+    def __init__(self, api_client):
+        self._api_client = api_client
+
+    def get_wireguard_config(self) -> Tuple[bool, Union[Dict[str, Any], List[Dict[str, Any]]]]:
+        endpoint = "/wireguard/config"
+
+        return self._api_client.get(endpoint)
+
+    def create_wireguard_config(self, config: Dict[str, Any]) -> Tuple[bool, Union[Dict[str, Any], List[Dict[str, Any]]]]:
+        endpoint = "/wireguard/config"
+
+        data = {"data": config}
+
+        return self._api_client.post(endpoint, data=data)
+
+    def update_wireguard_config(self, config: Dict[str, Any]) -> Tuple[bool, Union[Dict[str, Any], List[Dict[str, Any]]]]:
+        endpoint = "/wireguard/config"
+
+        data = {"data": config}
+
+        return self._api_client.put(endpoint, data=data)
+
+    def delete_wireguard_config(self, config: Dict[str, Any]) -> Tuple[bool, Union[Dict[str, Any], List[Dict[str, Any]]]]:
+        endpoint = "/wireguard/config"
+
+        data = {"data": config}
+
+        return self._api_client.delete(endpoint, data=data)
+
+    def get_wireguard_config_by_id(self, wireguard_id: str) -> Tuple[bool, Union[Dict[str, Any], List[Dict[str, Any]]]]:
+        endpoint = f"/wireguard/config/{wireguard_id}"
+
+        return self._api_client.get(endpoint)
+
+    def update_wireguard_config_by_id(self, wireguard_id: str, config: Dict[str, Any]) -> Tuple[bool, Union[Dict[str, Any], List[Dict[str, Any]]]]:
+        endpoint = f"/wireguard/config/{wireguard_id}"
+
+        data = {"data": config}
+
+        return self._api_client.put(endpoint, data=data)
+
+    def delete_wireguard_config_by_id(self, wireguard_id: str) -> Tuple[bool, Union[Dict[str, Any], List[Dict[str, Any]]]]:
+        endpoint = f"/wireguard/config/{wireguard_id}"
+
+        return self._api_client.delete(endpoint)
+
+    def wireguard_actions_generate_keys(self) -> Tuple[bool, Union[Dict[str, Any], List[Dict[str, Any]]]]:
+        endpoint = "/wireguard/actions/generate_keys"
+
+        return self._api_client.post(endpoint)
