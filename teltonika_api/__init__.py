0cef188e22aa4dfce43b8a07cb7df3d1ae1b6cbb
Iker García Calviño <iker.gcalvino@udc.es>
2025-05-02 10:27:15 +0200

    Add router device classes to the teltonika_api module

---
+++ teltonika_api/__init__.py
@@ -0,0 +1,19 @@
+from .devices.routers import (
+    Router,
+    RUT200,
+    RUT240,
+    RUT241,
+    RUT956,
+    RUTX08,
+    RUTX50,
+)
+
+__all__ = [
+    "Router",
+    "RUT200",
+    "RUT240",
+    "RUT241",
+    "RUT956",
+    "RUTX08",
+    "RUTX50",
+]
