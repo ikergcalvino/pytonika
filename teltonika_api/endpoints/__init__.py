b20bd8344ff385e7caf126cac0888b2fb7063f19
Iker García Calviño <iker.gcalvino@udc.es>
2025-05-02 08:36:52 +0200

    Add initial endpoint definitions for Authentication, Firmware, Interfaces, Unauthorized, Users, and WireGuard

---
+++ teltonika_api/endpoints/__init__.py
@@ -0,0 +1,15 @@
+from .authentication import Authentication
+from .firmware import Firmware
+from .interfaces import Interfaces
+from .unauthorized import Unauthorized
+from .users import Users
+from .wireguard import WireGuard
+
+__all__ = [
+    "Authentication",
+    "Firmware",
+    "Interfaces",
+    "Unauthorized",
+    "Users",
+    "WireGuard",
+]
