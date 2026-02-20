75b436b9feafd27cfee076b18f556580fd5b0253
Iker García Calviño <iker.gcalvino@udc.es>
2026-02-20 22:59:44 +0100

    Add support for additional Teltonika device models including ATRM50, CAP700, RUT145, RUT276, RUT281, RUT981, RUT986, RUTC41, RUTM16, RUTM20, and TAP400

--- pytonika/devices/access_points/__init__.py
+++ pytonika/devices/access_points/__init__.py
@@ -1,9 +1,11 @@
 from .access_point import AccessPoint
 from .tap100 import TAP100
 from .tap200 import TAP200
+from .tap400 import TAP400
 
 __all__ = [
     "AccessPoint",
     "TAP100",
     "TAP200",
+    "TAP400",
 ]
--- pytonika/devices/routers/__init__.py
+++ pytonika/devices/routers/__init__.py
@@ -1,4 +1,6 @@
 from .router import Router
+from .atrm50 import ATRM50
+from .cap700 import CAP700
 from .dap140 import DAP140
 from .dap142 import DAP142
 from .dap145 import DAP145
@@ -7,12 +9,15 @@ from .otd144 import OTD144
 from .otd500 import OTD500
 from .rut140 import RUT140
 from .rut142 import RUT142
+from .rut145 import RUT145
 from .rut200 import RUT200
 from .rut206 import RUT206
 from .rut240 import RUT240
 from .rut241 import RUT241
 from .rut260 import RUT260
 from .rut271 import RUT271
+from .rut276 import RUT276
+from .rut281 import RUT281
 from .rut300 import RUT300
 from .rut301 import RUT301
 from .rut360 import RUT360
@@ -24,11 +29,16 @@ from .rut951 import RUT951
 from .rut955 import RUT955
 from .rut956 import RUT956
 from .rut976 import RUT976
+from .rut981 import RUT981
+from .rut986 import RUT986
+from .rutc41 import RUTC41
 from .rutc50 import RUTC50
 from .rutm08 import RUTM08
 from .rutm09 import RUTM09
 from .rutm10 import RUTM10
 from .rutm11 import RUTM11
+from .rutm16 import RUTM16
+from .rutm20 import RUTM20
 from .rutm30 import RUTM30
 from .rutm31 import RUTM31
 from .rutm50 import RUTM50
@@ -50,6 +60,8 @@ from .tcr100 import TCR100
 
 __all__ = [
     "Router",
+    "ATRM50",
+    "CAP700",
     "DAP140",
     "DAP142",
     "DAP145",
@@ -58,12 +70,15 @@ __all__ = [
     "OTD500",
     "RUT140",
     "RUT142",
+    "RUT145",
     "RUT200",
     "RUT206",
     "RUT240",
     "RUT241",
     "RUT260",
     "RUT271",
+    "RUT276",
+    "RUT281",
     "RUT300",
     "RUT301",
     "RUT360",
@@ -75,11 +90,16 @@ __all__ = [
     "RUT955",
     "RUT956",
     "RUT976",
+    "RUT981",
+    "RUT986",
+    "RUTC41",
     "RUTC50",
     "RUTM08",
     "RUTM09",
     "RUTM10",
     "RUTM11",
+    "RUTM16",
+    "RUTM20",
     "RUTM30",
     "RUTM31",
     "RUTM50",
